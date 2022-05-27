3b30c6af05ff183083d0c3abde760a356c9630fb
Chris Roberts <croberts@lewiscommunications.com>
2022-05-26 23:43:01 -0500

    Add initial routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use Squarepeg\Http\Controllers\SquareController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(SquareController::class)->group(function () {
+    Route::get('sum_squares', 'sumSquares');
+    Route::get('square_sums', 'squareSums');
 });
