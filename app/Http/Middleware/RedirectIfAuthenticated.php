55ee77695999b81328f7b002a2037b1018f40fbf
Chris Roberts <croberts@lewiscommunications.com>
2022-05-26 22:57:17 -0500

    Modify namespace & initial defaults

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Squarepeg\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
+use Squarepeg\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Squarepeg\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        // 'Squarepeg\Models\Model' => 'Squarepeg\Policies\ModelPolicy',
     ];
 
     /**
